c59b90d566962980af93ddfaf157ac8fa8361748
k3b4bb <enriquemartin1402@gmail.com>
2024-12-24 09:45:24 +0100

    Set up development environment

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { 
+      globals: globals.node 
+    },
+    plugins: {
+      prettier: prettierPlugin
+    },
+    rules: {
+      "prettier/prettier": "error"
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier
+];

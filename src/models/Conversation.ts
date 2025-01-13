1a912bf121000ff2f2f40e1220fab33f07f317f1
DeusExDev <enriquemartin1402@gmail.com>
2025-01-13 21:28:45 +0100

    Add validation to Conversation Model

--- src/models/Conversation.ts
+++ src/models/Conversation.ts
@@ -1,30 +1,52 @@
 import { Schema, model } from 'mongoose';
 import { IConversation } from '../types/conversation.types.js';
+import { AiName } from '../types/ai.types.js';
 
 const conversationSchema = new Schema<IConversation>({
   userId: {
     type: String,
     required: true,
     ref: 'User',
+    trim: true,
+    validate: {
+      validator: (walletAddress: string) => {
+        return /^[1-9A-HJ-NP-Za-km-z]{32,44}$/.test(walletAddress);
+      },
+      message: 'Ivalid wallet address format',
+    },
   },
   title: {
     type: String,
     required: false,
+    maxlength: [55, 'Tittle is too long'],
+    trim: true,
   },
   aiName: {
     type: String,
     required: true,
     default: 'MACHINA',
+    enum: {
+      values: ['MACHINA'] as AiName[],
+      message: 'Invalid AI assistant name',
+    },
   },
   lastMessageAt: {
     type: Date,
     default: Date.now,
     required: true,
+    validate: {
+      validator: (date: Date) => date <= new Date(),
+      message: 'Last message timestamp cannot be in the future',
+    },
   },
   createdAt: {
     type: Date,
     default: Date.now,
     required: true,
+    validate: {
+      validator: (date: Date) => date <= new Date(),
+      message: 'Creation timestamp cannot be in the future',
+    },
   },
 });
 
